0e0cec53f8aaeebd5e1cf358d626a978cdf33559
shaileshmsk <msk.sunny96@gmail.com>
2022-12-19 08:01:38 +0000

    Moving Files

---
+++ Local-websetup/1firstscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Welcome to the bash scripts."
+echo
+
+echo "The uptime of this system is: "
+uptime
+echo
+echo "######################################"
+echo "Memory utilization: "
+free -m
+echo
+echo "######################################"
+echo "Disk utilizatoin: "
+df -h
+echo
+echo "######################################"
